a8bcaa8828213c72656734a89739a46cf24bfc0d
zloyaloha <cutevf17@gmail.com>
2024-12-27 13:35:34 +0300

    merging develop. release v.1
    Signed-off-by: zloyaloha <cutevf17@gmail.com>

---
+++ scripts/unpack_env.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ZIP_FILES=(
+    "data/s3_env.zip"
+)
+
+DEST_DIRS=(
+    "core/env"
+)
+
+# Перебор архивов и директорий
+for i in "${!ZIP_FILES[@]}"; do
+    ZIP_FILE="${ZIP_FILES[i]}"
+    DEST_DIR="${DEST_DIRS[i]}"
+
+# Распаковка архива
+unzip "$ZIP_FILE" -d "$DEST_DIR"
+
+    # Проверка существования директории для распаковки
+    if [ ! -d "$DEST_DIR" ]; then
+        echo "Директория '$DEST_DIR' не существует. Создаю её."
+        mkdir -p "$DEST_DIR"
+    fi
+
+    # Распаковка архива
+    unzip "$ZIP_FILE" -d "$DEST_DIR"
+
+    # Проверка на успешность распаковки
+    if [ $? -eq 0 ]; then
+        echo "Архив '$ZIP_FILE' успешно распакован в '$DEST_DIR'."
+    else
+        echo "Ошибка при распаковке архива '$ZIP_FILE'."
+    fi
+done
